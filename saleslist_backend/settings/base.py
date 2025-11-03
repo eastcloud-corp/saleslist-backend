40339c2310f0390404515c3baa4f64e5a39babb4
ko-kikuchi <ko.kikuchi.718@gmail.com>
2025-11-04 07:11:44 +0900

    INV-001: Fix admin static URL

--- saleslist_backend/settings/base.py
+++ saleslist_backend/settings/base.py
@@ -112,7 +112,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
